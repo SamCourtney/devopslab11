9bd03f18a473a65814f8dde2dcd4c879f38d75b1
Sam-Courtney1 <samcourtney3722@gmail.com>
2024-11-29 12:45:14 +0000

    Working test file

---
+++ test_sudoku.py
@@ -0,0 +1,40 @@
+import unittest
+from sudoku import sudoku_solver, validate_board
+
+class TestSudokuSolver(unittest.TestCase):
+    def test_valid_board(self):
+        board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        solved = sudoku_solver(board)
+        self.assertIsNotNone(solved)
+
+    def test_unsolvable_board(self):
+        board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 1],  # Invalid input
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        self.assertIsNone(sudoku_solver(board))
+
+    def test_invalid_input(self):
+        board = "invalid"
+        with self.assertRaises(ValueError):
+            sudoku_solver(board)
+
+if __name__ == "__main__":
+    unittest.main()
