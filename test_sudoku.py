91672c1d13ebc245f915f06a00a60e2020bfdd22
Sam-Courtney1 <samcourtney3722@gmail.com>
2024-11-29 15:20:21 +0000

    Changes to test cases to include invalid input 3x3 square

--- test_sudoku.py
+++ test_sudoku.py
@@ -87,20 +87,15 @@ class TestSudokuSolver(unittest.TestCase):
         self.assertEqual(solved, board)
    
 
+    def test_invalid_board_size(self):
+        board = [
+        [5, 3, 0],
+        [6, 0, 0],
+        [0, 9, 8]
+    ]
+        with self.assertRaises(ValueError):
+            sudoku_solver(board)
+
 
 if __name__ == "__main__":
-    unittest.main()
-def test_already_solved_board(self):
-    board = [
-        [5, 3, 4, 6, 7, 8, 9, 1, 2],
-        [6, 7, 2, 1, 9, 5, 3, 4, 8],
-        [1, 9, 8, 3, 4, 2, 5, 6, 7],
-        [8, 5, 9, 7, 6, 1, 4, 2, 3],
-        [4, 2, 6, 8, 5, 3, 7, 9, 1],
-        [7, 1, 3, 9, 2, 4, 8, 5, 6],
-        [9, 6, 1, 5, 3, 7, 2, 8, 4],
-        [2, 8, 7, 4, 1, 9, 6, 3, 5],
-        [3, 4, 5, 2, 8, 6, 1, 7, 9]
-    ]
-    solved = sudoku_solver(board)
-    self.assertEqual(solved, board)
+    unittest.main()      
